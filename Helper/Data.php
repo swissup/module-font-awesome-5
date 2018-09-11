a244fd93bc7779d2a72f6b4044a69be4b2833d28
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-09-11 15:07:28 +0300

    Added ability to enable pro CDN

--- Helper/Data.php
+++ Helper/Data.php
@@ -17,12 +17,27 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      * @var string
      */
+    const CONFIG_PATH_USE_PRO = 'swissup_fontawesome5/general/use_pro';
+
+    /**
+     * @var string
+     */
     const ASSET_REMOTE_URL = 'https://use.fontawesome.com/releases/v5.3.1/css/all.css';
 
     /**
      * @var string
      */
     const ASSET_LOCAL_URL = 'Swissup_FontAwesome5::fontawesome-free-5.3.1-web/css/all.min.css';
+
+    /**
+     * @var string
+     */
+    const ASSET_PRO_REMOTE_URL = 'https://pro.fontawesome.com/releases/v5.3.1/css/all.css';
+
+    /**
+     * @var string
+     */
+    const ASSET_PRO_LOCAL_URL = 'Swissup_FontAwesome5::fontawesome-pro-5.3.1-web/css/all.min.css';
 
     /**
      * Retrieve isFontAwesomeEnabled flag
@@ -44,8 +59,21 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
      */
     public function canUseCdn()
     {
-        return $this->scopeConfig->getValue(
+        return $this->canUsePro() || $this->scopeConfig->getValue(
             self::CONFIG_PATH_USE_CDN,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Retrieve canUsePro flag
+     *
+     * @return boolean
+     */
+    public function canUsePro()
+    {
+        return $this->scopeConfig->getValue(
+            self::CONFIG_PATH_USE_PRO,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
     }
@@ -72,12 +100,10 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
      */
     public function getRemoteAsset()
     {
-        $url = self::ASSET_REMOTE_URL;
-        $type = 'css';
         $asset = $this->getBaseAsset();
         $asset->addData([
-            'url'  => $url,
-            'type' => $type
+            'url'  => $this->canUsePro() ? self::ASSET_PRO_REMOTE_URL : self::ASSET_REMOTE_URL,
+            'type' => 'css'
         ]);
         return $asset;
     }
@@ -91,7 +117,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         $asset = $this->getBaseAsset();
         $asset->addData([
-            'url'  => self::ASSET_LOCAL_URL,
+            'url'  => $this->canUsePro() ? self::ASSET_PRO_LOCAL_URL : self::ASSET_LOCAL_URL,
             'type' => 'css'
         ]);
         return $asset;
